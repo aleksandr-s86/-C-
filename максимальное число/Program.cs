36950015621d93c877633e6dea416fdaa0f04caa
Aleksandr <aleksandr-s86>
2023-02-12 20:33:28 +0300

    нахождение максимального числа

---
+++ максимальное число/Program.cs
@@ -0,0 +1,19 @@
+﻿// находим максимальное из трёх чисел
+Console.WriteLine("Введите первое число");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int number2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число");
+int number3 = int.Parse(Console.ReadLine()!);
+int max = number1;
+if(number2>number3)
+{
+Console.WriteLine("максимальное число -"+number2);
+}
+if(number3>number1)
+{
+Console.WriteLine("максимальное число -"+number3);
+}
+else
+{Console.WriteLine("максимальное число -"+number1);
+}
