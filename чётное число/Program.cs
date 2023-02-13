3c6acdcc41e3b9f61acd97349735fdf46453f9f3
Aleksandr <aleksandr-s86>
2023-02-13 18:02:47 +0300

    определение чётного числа

---
+++ чётное число/Program.cs
@@ -0,0 +1,12 @@
+﻿// Чётное число
+Console.WriteLine("Введите число");
+int number =int.Parse(Console.ReadLine()!);
+int result = number/2;
+if (number%2==0)
+{
+    Console.WriteLine("четное");
+}
+else
+{
+    Console.WriteLine("нечётное");
+}
